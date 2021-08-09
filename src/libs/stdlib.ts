add31d1211caa199462579b07dc15dab5efa39a4
uellenberg <JonahUellenberg@hotmail.com>
2021-08-08 22:25:20 -0700

    Add len and getDigit functions

--- src/libs/stdlib.ts
+++ src/libs/stdlib.ts
@@ -1,5 +1,7 @@
 export default
-`//All this function does is loop through every number < floor(sqrt(a)), then checks if a is divisible by it.
+`//Returns 1 if the input is prime, and 0 otherwise.
+//
+//All this function does is loop through every number < floor(sqrt(a)), then checks if a is divisible by it.
 //If it is, then it adds 1 to the sum, otherwise zero. This is then put through select, which will make it 0
 //if prime and 1 otherwise. Next, it's put through not, to make it the opposite.
 inline function isPrime(a) {
@@ -12,4 +14,17 @@ inline function isPrime(a) {
     };
 
     state = not(select(state));
+}
+
+//Returns the number of digits in the input.
+inline function len(a){
+    state = floor(log(a));
+}
+
+//Extracts the nth digit (starting at 0) from a number. The first input is the number and the second is the place of the digit.
+inline function getDigit(a, b) {
+    const pt1 = a/10^(len(a)-b);
+    const pt2 = a/10^(len(a)-b+1);
+    
+    state = floor(pt1-floor(pt2)*10);
 }`;
