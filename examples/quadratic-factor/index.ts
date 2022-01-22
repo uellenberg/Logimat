8040851ff9ffb5896aee83c878714798d089482b
uellenberg <JonahUellenberg@hotmail.com>
2022-01-21 17:53:19 -0800

    Update examples

--- examples/quadratic-factor/index.ts
+++
@@ -1,7 +0,0 @@
-import {Compile} from "../../src";
-import * as fs from "fs";
-import * as Path from "path";
-
-const func = Compile(fs.readFileSync(Path.join(__dirname, "quadratic-factor.lm"), "utf-8"));
-
-process.stdout.write(func);
