15689a2852c55a221559a079ed5fa0a8119beb59
uellenberg <JonahUellenberg@hotmail.com>
2024-03-29 12:55:32 -0700

    Move some stack code into a separate file

--- src/parser/compiler.ts
+++ src/parser/compiler.ts
@@ -31,6 +31,7 @@ import path from "path";
 import {HandleName, isNumeric, opMap} from "./util";
 import piecewiseOps from "../libs/piecewiseOps";
 import {createTemplates} from "./templates";
+import {CompileStackFunction, createExecutionPoint, generateCallFunction, GetStackSelector} from "./stack";
 
 /**
  * Compiles Logimat to a math function (or multiple). Each function/variable will be on a separate line.
@@ -575,166 +576,7 @@ const InternalCompile = (useTex: boolean, tree: OuterDeclaration[], inlines: Rec
                     // Display doesn't make sense for stack functions, so reset it.
                     CompileDisplay(display);
 
-                    data.parentStackPrefix = declaration.name;
-                    data.stackContext = true;
-                    // The +2 is needed to account for the returnStackNum, and to
-                    // move it one past the end of the stack.
-                    data.stackOffset = declaration.args.length + 2;
-
-                    // Add virtual variables for all the arguments.
-                    // The stack looks like [stackNum, stackFramePtr, ..., returnStackNum, returnPtr, arg1, ...],
-                    // so we need to access the first argument like stack[1] + 2.
-                    for(let i = 0; i < functionDeclaration.args.length; i++) {
-                        const argName = functionDeclaration.args[i];
-
-                        let varIdx = data.addrIdx.value++;
-                        data.variableMap[argName] = {idx: varIdx, variable: false};
-                        data.vars[varIdx] = "array_idx(s_tack, 2) + " + (i + 2);
-                    }
-
-                    // First, we need one compile pass to create the stack numbers
-                    // for each break point.
-                    // This will produce meaningless output.
-                    // This clone is needed because variables have unique IDs.
-                    let tempData = {
-                        ...data,
-                        ...structuredClone({
-                            vars: data.vars,
-                            variableMap: data.variableMap,
-                            varIdx: data.varIdx,
-                            addrIdx: data.addrIdx,
-                            stackStateMap: data.stackStateMap,
-                            stackFunctionMap: data.stackFunctionMap,
-                            stackNextStateMap: data.stackNextStateMap,
-                            stackIdx: data.stackIdx,
-                            globalStackNumber: data.globalStackNumber,
-                            shouldExit: data.shouldExit,
-                        })
-                    };
-
-                    let stackNumVar = tempData.addrIdx.value++;
-                    tempData.vars[stackNumVar] = "-1";
-                    tempData.variableMap["stacknum"] = {idx: stackNumVar, variable: false};
-                    tempData.preCompile = true;
-
-                    CompileBlock([
-                        // This is here to prevent if statements from complaining
-                        // about there not being a state set.
-                        {type: "var", name: "state", expr: {type: "v", args: ["s_tate"]}},
-                        ...functionDeclaration.block,
-                    ], tempData, "", 0 /* state */, true, out);
-
-                    // Now, we need to add the final execution step.
-                    // This is explained below, but it's essentially the code
-                    // that makes the function actually return.
-                    let finalStackNum = tempData.globalStackNumber.value++;
-                    let finalStackIdx = tempData.stackIdx.value++;
-                    tempData.stackStateMap[tempData.parentStackPrefix + "_" + finalStackIdx] = finalStackNum;
-
-                    // Determine the number of break points the function has.
-                    let newVersions = tempData.globalStackNumber.value - data.globalStackNumber.value;
-                    let initialStateNum = data.globalStackNumber.value;
-
-                    // Now, we need to bring the relevant fields over.
-                    data.stackStateMap = tempData.stackStateMap;
-                    data.globalStackNumber = tempData.globalStackNumber;
-                    data.stackNextStateMap = tempData.stackNextStateMap;
-                    data.stackFunctionMap = tempData.stackFunctionMap;
-                    data.callFunctionsEmitted = tempData.callFunctionsEmitted;
-                    data.stackFunctionMap = tempData.stackFunctionMap;
-
-                    // Now, the function needs to be compiled for each different breakpoint
-                    // that exists.
-                    // This will create different versions to handle each part of the execution.
-                    // There will also be a built-in return function that gets called for the last
-                    // one, and which will be assigned to finalStackNum.
-                    stackNumVar = data.addrIdx.value++;
-                    data.variableMap["stacknum"] = {idx: stackNumVar, variable: false};
-
-                    const firstStackNum = data.stackStateMap[declaration.name + "_0"];
-
-                    // We need to map each state to its next state as a default value.
-                    // This will ignore our final one, as it doesn't have a next state to map to.
-                    const numToName: Record<number, string> = Object.fromEntries(Object.entries(data.stackStateMap).map(val => val.reverse()));
-                    let nextStateSelector = "const s_tack1 = a_dv(s_tack, ";
-                    for(let i = 0; i < newVersions; i++) {
-                        // i == 0 is a special case for the first breakpoint.
-                        const stackNum = i === 0 ? firstStackNum : (initialStateNum + (i - 1));
-
-                        if(i === 0) {
-                            nextStateSelector += "if(stacknum == " + stackNum + ")";
-                        } else {
-                            nextStateSelector += "else if(stacknum == " + stackNum + ")";
-                        }
-
-                        nextStateSelector += "{" + data.stackStateMap[data.stackNextStateMap[numToName[stackNum]]] + "}";
-                    }
-                    // This last run runs for return, which doesn't make use of the
-                    // current stack num, so it won't matter anyway.
-                    nextStateSelector += "else { 0 });";
-                    // This is broken out so that it can be reset to at the end of each breakpoint.
-                    nextStateSelector += "state = s_tack1;";
-
-                    const newStateSelectorParsed = GetStatementsTree(nextStateSelector);
-                    const functionStatements = [...newStateSelectorParsed, ...functionDeclaration.block];
-
-                    for(let i = 0; i <= newVersions; i++) {
-                        // We need to use cloned data to avoid
-                        // the state from getting messed up with multiple
-                        // iterations.
-                        const clonedData = {
-                            ...data,
-                            ...structuredClone({
-                                vars: data.vars,
-                                variableMap: data.variableMap,
-                                varIdx: data.varIdx,
-                                addrIdx: data.addrIdx,
-                                stackStateMap: data.stackStateMap,
-                                stackFunctionMap: data.stackFunctionMap,
-                                stackNextStateMap: data.stackNextStateMap,
-                                stackIdx: data.stackIdx,
-                                globalStackNumber: data.globalStackNumber,
-                                callFunctionEmitted: data.callFunctionsEmitted,
-                                shouldExit: data.shouldExit,
-                            })
-                        };
-
-                        // 0 is a special case for the first execution point.
-                        const name = functionDeclaration.name + "_" + i;
-                        const stackNum = i === 0 ? firstStackNum : initialStateNum + (i - 1);
-
-                        clonedData.vars[stackNumVar] = stackNum.toString();
-
-                        if(i === newVersions) {
-                            // state = r_et(state);
-                            const returnCode: Statement[] = [
-                                ...functionStatements,
-                                {
-                                    type: "var",
-                                    name: "state",
-                                    expr: {
-                                        type: "f",
-                                        args: [
-                                            "r_et",
-                                            [
-                                                {type: "v", args: ["state"]}
-                                            ]
-                                        ]
-                                    }
-                                }
-                            ];
-                            out.push(HandleName(name) + "(s_{tack})=" + SimplifyExpression(CompileBlock(returnCode, clonedData, "", 0 /* state */, true, out), useTex, strict, names.concat("s_tack", "a_dv", "r_et").concat(data.callFunctionsEmitted.map(call => "c_all" + call)), simplificationMap));
-                        } else {
-                            out.push(HandleName(name) + "(s_{tack})" + "=" + SimplifyExpression(CompileBlock(functionStatements, clonedData, "", 0 /* state */, true, out), useTex, strict, names.concat("s_tack", "a_dv", "r_et").concat(data.callFunctionsEmitted.map(call => "c_all" + call)), simplificationMap));
-                        }
-
-                        data.stackStateMap = clonedData.stackStateMap;
-                        data.globalStackNumber = clonedData.globalStackNumber;
-                        data.stackNextStateMap = clonedData.stackNextStateMap;
-                        data.stackFunctionMap = clonedData.stackFunctionMap;
-                        data.callFunctionsEmitted = clonedData.callFunctionEmitted;
-                        data.stackFunctionMap[stackNum] = name;
-                    }
+                    CompileStackFunction(data, functionDeclaration, out, useTex, strict, names, simplificationMap);
 
                     break;
                 }
@@ -819,7 +661,7 @@ const GetTree = (input: string) : ParserOutput => {
     return semantic(match).parse();
 }
 
-const GetStatementsTree = (input: string) : Statement[] => {
+export const GetStatementsTree = (input: string) : Statement[] => {
     if(input.trim() === "") return [];
 
     const match = grammar.match(input, "InnerDeclarations");
@@ -875,7 +717,7 @@ const GetDeclaredNames = (tree: OuterDeclaration[]) : string[] => {
     return output;
 }
 
-const CompileBlock = (input: Statement[], data: CompileData, defaultOut: string, curVar: number, requireOutput: boolean, compilerOutput: string[]) : string => {
+export const CompileBlock = (input: Statement[], data: CompileData, defaultOut: string, curVar: number, requireOutput: boolean, compilerOutput: string[]) : string => {
     let out = defaultOut;
     let newVars = {
         // State (0) should always exist.
@@ -1019,51 +861,11 @@ const CompileBlock = (input: Statement[], data: CompileData, defaultOut: string,
                     throw new Error("Only stack functions can be ran!");
                 }
 
-                // This needs to be split into its own subfunction.
-                const stackIdx = data.stackIdx.value++;
-                const stackName = data.parentStackPrefix + "_" + stackIdx;
-
-                // We need this to have the list of next steps, and so that
-                // the function returns to the step after this.
-                const nextStepName = data.parentStackPrefix + "_" + (stackIdx + 1);
-                data.stackNextStateMap[stackName] = nextStepName;
-                const nextStepNum = data.stackStateMap[nextStepName];
-
-                let stackNum: number;
-                if(stackName in data.stackStateMap) {
-                    stackNum = data.stackStateMap[stackName];
-                } else {
-                    stackNum = data.globalStackNumber.value++;
-                    data.stackStateMap[stackName] = stackNum;
-                }
+                let {nextStepNum, stackNum} = createExecutionPoint(data);
 
                 // Use the call function if it already exists,
                 // otherwise generate if.
-                if(!data.callFunctionsEmitted.includes(statement.args.length)) {
-                    // We need to generate a piece of code that does the following:
-                    // - Puts a returnNum onto the stack.
-                    // - Puts a returnStackPointer onto the stack.
-                    // - Puts all of its arguments onto the stack.
-                    // - Adjusts the stack
-                    let callCode = `let newStack = s_tack;
-                    newStack[newStack[2] + ${data.stackOffset}] = b_ack;
-                    newStack[newStack[2] + ${data.stackOffset} + 1] = newStack[2];`;
-
-                    for(let i = 0; i < statement.args.length; i++) {
-                        callCode += `newStack[newStack[2] + ${data.stackOffset} + ${i} + 2] = a_rg${i};`
-                    }
-
-                    callCode += `newStack[2] = newStack[2] + 2 + ${statement.args.length};`;
-                    callCode += `newStack[1] = n_um;`;
-                    callCode += "newStack";
-
-                    // Compile as a stand-alone block.
-                    const newData = cleanData(data, []);
-                    const compiled = CompileBlock(GetStatementsTree(callCode), newData, "", 0 /* state */, true, compilerOutput);
-                    compilerOutput.push(HandleName("c_all" + statement.args.length) + "(s_{tack},n_{um},b_{ack}" + (statement.args.length === 0 ? "" : ",") + statement.args.map((_, idx) => HandleName("a_rg" + idx)).join(",") + ")=" + SimplifyExpression(compiled, newData.useTex, newData.strict, newData.names.concat("s_tack", "n_um", "b_ack", "a_rrset", ...statement.args.map((_, idx) => "a_rg" + idx)), newData.simplificationMap));
-
-                    data.callFunctionsEmitted.push(statement.args.length);
-                }
+                generateCallFunction(data, statement.args.length, compilerOutput);
 
                 // There's no reason for us to compile anything if this isn't requested.
                 // The code above is required to run, however.
@@ -1092,7 +894,7 @@ const CompileBlock = (input: Statement[], data: CompileData, defaultOut: string,
                 // The stack num has already been generated here, so we can retrieve it.
                 const callStackNum = data.stackStateMap[statement.name + "_0"];
 
-                let runCode = `state = c_all${statement.args.length}(state,${callStackNum},${nextStepNum}`;
+                let runCode = `state = c_all${statement.args.length}(state,${callStackNum},${nextStepNum},${data.stackOffset}`;
                 if(statement.args.length !== 0) runCode += ",";
                 for(let i = 0; i < statement.args.length; i++) {
                     runCode += `${CompileExpression(statement.args[i], {
@@ -1135,20 +937,7 @@ const CompileBlock = (input: Statement[], data: CompileData, defaultOut: string,
                     throw new Error("Returns can only be happen inside of a state function!");
                 }
 
-                // This needs to be split into its own subfunction.
-                const stackIdx = data.stackIdx.value++;
-                const stackName = data.parentStackPrefix + "_" + stackIdx;
-
-                // We need this to have the list of next steps.
-                data.stackNextStateMap[stackName] = data.parentStackPrefix + "_" + (stackIdx + 1);
-
-                let stackNum: number;
-                if(stackName in data.stackStateMap) {
-                    stackNum = data.stackStateMap[stackName];
-                } else {
-                    stackNum = data.globalStackNumber.value++;
-                    data.stackStateMap[stackName] = stackNum;
-                }
+                let {stackNum} = createExecutionPoint(data);
 
                 // There's no reason for us to compile anything if this isn't requested.
                 // The code above is required to run, however.
@@ -1469,7 +1258,7 @@ const CompileExpression = (expression: Expression, data: CompileData, compilerOu
  * Resets fields of a `CompileData` so that it can be used
  * on a different declaration.
  */
-const cleanData = (data: CompileData, names: string[]) : CompileData => {
+export const cleanData = (data: CompileData, names: string[]) : CompileData => {
     return {
         inlines: data.inlines,
         templates: data.templates,
@@ -1498,34 +1287,12 @@ const cleanData = (data: CompileData, names: string[]) : CompileData => {
     };
 }
 
-const GetStackSelector = (data: CompileData) : string => {
-    let code = "";
-
-    const entries = Object.entries(data.stackFunctionMap);
-    for(let i = 0; i < entries.length; i++) {
-        const [stackNum, functionName] = entries[i];
-        const cond = "s_tack[1] == " + stackNum;
-        const body = `{ ${functionName}(s_tack) }`;
-
-        if(i === 0) {
-            code += `if(${cond}) ${body}`;
-        } else {
-            code += `else if(${cond}) ${body}`;
-        }
-    }
-
-    code += "else { s_tack }";
-
-    const compiled = CompileBlock(GetStatementsTree(code), data, "", 0 /* state */, true, []);
-    return "r_{un}(s_{tack})=" + SimplifyExpression(compiled, data.useTex, data.strict, data.names.concat("s_tack", ...Object.keys(data.stackStateMap)), data.simplificationMap);
-}
-
 interface Inline {
     function: boolean;
     value: OuterDeclaration;
 }
 
-interface CompileData {
+export interface CompileData {
     inlines: Record<string, Inline>;
     templates: Record<string, TemplateFunction>;
     state: TemplateState;
